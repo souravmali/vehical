a7013740805c86537c48c1abde95bf3edbdfaf6d
souravmali <souravmali6516@gmail.com>
2018-07-29 12:33:12 +0530

    new class

---
+++ vclass.py
@@ -0,0 +1,23 @@
+class vehical():
+      
+   	no_of wheels = 0
+	speed=0
+	weight=0
+	mileage=none
+	
+	def __init__(self,no_of_wheels,	speed,weight,mileage)
+		
+		self.no_of_wheels=no_of_wheels
+		self.speed=spped
+		self.weight=weight
+		self.mileage=mileage
+	
+	
+	def go_forword(self)
+		print("start to  move forword"+no_of_wheels)
+	def reverse(self)
+		print("start to  move back"+no_of_wheels)
+	def stop(self)
+		print("start to  move back"+no_of_wheels)
+	
+	
